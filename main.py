e82e507434bf2bed85a72cf7b194b12f9258e777
Orçun <orcuncakar48@gmail.com>
2023-07-10 14:42:55 +0300

    Provided Random Movement

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ turtle_view.shape("turtle")
 
 turtle_view.penup()
 turtle_view.speed(2)
-count=0
-while count<51:
-    count+=1
-    if (turtle_view.xcor()>-4000 and turtle_view.xcor()<4000) and \
-            (turtle_view.ycor()>- 4000 and turtle_view.ycor()<4000):
-        turtle_view.forward(random.randint(70,150))
-        turtle_view.right(random.randint(0,360))
+count = 0
+while count < 51:
+    count += 1
+    if (turtle_view.xcor() > -4000 and turtle_view.xcor() < 4000) and \
+            (turtle_view.ycor() > - 4000 and turtle_view.ycor() < 4000):
+        turtle_view.hideturtle()
+        turtle_view.forward(random.randint(70, 300))
+        turtle_view.showturtle()
+        turtle_view.right(random.randint(140, 360))
 
 
+def turtle_return_home():
+    turtle_view.home()
 
 
-turtle.done()
+drawing_board.onkey(fun=turtle_return_home, key="h")
+
+turtle.mainloop()
